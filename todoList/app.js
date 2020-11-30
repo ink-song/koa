b4b6b7f8cceadecec6c0c066d8049a80ba741395
奥特曼有好多鱼 <15519586771@163.com>
2020-11-30 15:29:39 +0800

    feat: 后端换肤功能

---
+++ todoList/app.js
@@ -0,0 +1,54 @@
+const Koa =  require('koa')
+const Router = require('koa-router')
+const router = new Router()
+const Swig = require('koa-swig')
+const StaticCache = require('koa-static-cache')
+const co = require('co')
+const app = new Koa()
+
+/**
+ * 实现换肤功能
+ *
+ */
+const data = {
+  skin: 'other',
+  users: [
+    {
+      name: 'nihao',
+      age: 12
+    },
+    {
+      name: 'nihao1',
+      age: 13
+    }
+  ]
+}
+
+/**
+ * 2. 设置模板引擎
+ */
+app.context.render = co.wrap(new Swig({
+  root: `${__dirname}/views`,
+  cache: false,
+  ext: 'html'
+}))
+
+/**
+ * 3.加载静态资源
+ */
+
+app.use(new StaticCache('./static', {
+  prefix: '/static',
+  gzip: true
+}))
+
+
+/**
+ * 1.设计路由
+ */
+router.get('/', async (ctx) => {
+  ctx.body = await ctx.render('index.html', {data})
+})
+
+app.use(router.routes())
+app.listen(80)
