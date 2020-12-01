afabab93ab88f1e405962f13b3e3f2425f864f18
奥特曼有好多鱼 <15519586771@163.com>
2020-12-01 14:34:21 +0800

    feat: 添加task

--- todoList/app.js
+++ todoList/app.js
@@ -4,6 +4,7 @@ const router = new Router()
 const Swig = require('koa-swig')
 const StaticCache = require('koa-static-cache')
 const co = require('co')
+const BodyParser = require('koa-bodyparser')
 const app = new Koa()
 
 /**
@@ -24,7 +25,6 @@ const data = {
     }
   ]
 }
-
 /**
  * 1. 设置模板引擎
  */
@@ -51,10 +51,42 @@ router.get('/', async (ctx) => {
   ctx.body = await ctx.render('index.html', {data})
 })
 
+/**
+ * 4.解析表单中body的数据
+ */
+app.use(new BodyParser())
+
 // add task
 router.get('/add', async (ctx) => {
   ctx.body = await ctx.render('add.html', {data})
 })
 
+// 提交表单
+/**
+ * url过长时，不推荐使用queryString的方式,
+ * post提交不会在历史记录中看到登陆信息
+ */
+router.get('/posttask', async (ctx) => {
+  ctx.body = '2222'
+})
+
+router.post('/posttask', async (ctx) => {
+  if (!ctx.request.body.title) {
+    ctx.body = await ctx.render('message', {
+      message: '请先输入标题',
+      href: 'javascript:history.back()'
+    })
+    return 
+  }
+  data.tasks.push({
+    id: ++data.maxId,
+    title: ctx.request.body.title,
+    done: false
+  })
+  ctx.body = await ctx.render('message', {
+    message: '添加成功',
+    href: '/'
+  })
+})
 app.use(router.routes())
 app.listen(80)
